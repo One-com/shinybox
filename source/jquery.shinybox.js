7fece4bc1869b0cc2179ef89c8639d5fcd6a9bb5
vineet kumar <vku@one.com>
2019-06-25 10:20:02 +0400

    5.1.0

--- source/jquery.shinybox.js
+++ source/jquery.shinybox.js
@@ -3,7 +3,7 @@
 @author       Constantin Saguin - @brutaldesign
 @link         http://csag.co
 @github       http://github.com/One-com/shinybox
-@version      5.0.4
+@version      5.1.0
 @license      MIT License
 
 ----------------------------------------------------------------------------------------------*/
